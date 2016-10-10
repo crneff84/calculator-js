c6cbabcce1e6b2bec3b9b3ad476513e70b301f53
Chance Neff and Martin Cartledge <chance+martin@>
2016-10-10 11:20:52 -0700

    add subtract, multiply, and divide functionality

--- js/calculator-interface.js
+++ js/calculator-interface.js
@@ -9,4 +9,31 @@ $(document).ready(function() {
     var output = simpleCalculator.add(firstAdd, secondAdd);
       $('#output').append("<p>" + output + "</p>");
   });
+
+  $('#subtract').submit(function(event) {
+    event.preventDefault();
+    var firstSubtract = parseInt($('#firstSubtract').val());
+    var secondSubtract = parseInt($('#secondSubtract').val());
+    var simpleCalculator = new Calculator();
+    var output = simpleCalculator.subtract(firstSubtract, secondSubtract);
+      $('#output').append("<p>" + output + "</p>");
+  });
+
+  $('#multiply').submit(function(event) {
+    event.preventDefault();
+    var firstMultiply = parseInt($('#firstMultiply').val());
+    var secondMultiply = parseInt($('#secondMultiply').val());
+    var simpleCalculator = new Calculator();
+    var output = simpleCalculator.multiply(firstMultiply, secondMultiply);
+      $('#output').append("<p>" + output + "</p>");
+  });
+
+  $('#divide').submit(function(event) {
+    event.preventDefault();
+    var firstDivide = parseInt($('#firstDivide').val());
+    var secondDivide = parseInt($('#secondDivide').val());
+    var simpleCalculator = new Calculator();
+    var output = simpleCalculator.divide(firstDivide, secondDivide);
+      $('#output').append("<p>" + output + "</p>");
+  });
 });
